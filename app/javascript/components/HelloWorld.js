0a4cc17b7d49804267ddf9dd9628529011ddf9a5
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-11-08 14:32:00 +0100

    Add an Action and a Reducer

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -1,16 +1,31 @@
 import React from "react"
-import PropTypes from "prop-types"
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+const GET_THINGS_REQUEST = 'GET_THINGS_REQUEST';
+
+function getThings() {
+  console.log('getThings() Action!!')
+  return {
+    type: GET_THINGS_REQUEST
+  };
+};
+
 class HelloWorld extends React.Component {
   render () {
     return (
-      <React.Fragment>
+      <>
         Greeting: {this.props.greeting}
-      </React.Fragment>
+        <button className="getThingsBtn" onClick={() => this.props.getThings()}>getThings</button>
+      </>
     );
   }
 }
 
-HelloWorld.propTypes = {
-  greeting: PropTypes.string
-};
-export default HelloWorld
+const structuredSelector = createStructuredSelector({
+  things: state => state.things,
+}); 
+
+const mapDispatchToProps = { getThings };
+
+export default connect(structuredSelector, mapDispatchToProps)(HelloWorld);
