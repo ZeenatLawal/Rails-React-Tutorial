c45f3ac135e59c67b0bdd2219adacb4631d410d2
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-11-08 14:46:07 +0100

    Use redux-thunk and fetch() to query API

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -3,11 +3,23 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 const GET_THINGS_REQUEST = 'GET_THINGS_REQUEST';
+const GET_THINGS_SUCCESS = 'GET_THINGS_SUCCESS';
 
 function getThings() {
   console.log('getThings() Action!!')
+  return dispatch => {
+    dispatch({ type: GET_THINGS_REQUEST });
+    return fetch(`v1/things.json`)
+      .then(response => response.json())
+      .then(json => dispatch(getThingsSuccess(json)))
+      .catch(error => console.log(error));
+  };
+};
+
+export function getThingsSuccess(json) {
   return {
-    type: GET_THINGS_REQUEST
+    type: GET_THINGS_SUCCESS,
+    json
   };
 };
 
