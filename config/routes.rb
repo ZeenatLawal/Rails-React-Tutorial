8192582c214ea9b6c3c0718383914db7baa13432
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-11-08 11:25:45 +0100

    Add Things controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :v1, defaults: { format: 'json' } do
+    get :things, to: 'things#index'
+  end
 end
