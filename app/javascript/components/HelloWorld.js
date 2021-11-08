bef08e6e8d404d756b4ad913fe2dcb6bff40ad95
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-11-08 15:37:31 +0100

    Use a Reasonable Directory Structure

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -2,27 +2,7 @@ import React from "react"
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { List } from 'semantic-ui-react'
-
-const GET_THINGS_REQUEST = 'GET_THINGS_REQUEST';
-const GET_THINGS_SUCCESS = 'GET_THINGS_SUCCESS';
-
-function getThings() {
-  console.log('getThings() Action!!')
-  return dispatch => {
-    dispatch({ type: GET_THINGS_REQUEST });
-    return fetch(`v1/things.json`)
-      .then(response => response.json())
-      .then(json => dispatch(getThingsSuccess(json)))
-      .catch(error => console.log(error));
-  };
-};
-
-export function getThingsSuccess(json) {
-  return {
-    type: GET_THINGS_SUCCESS,
-    json
-  };
-};
+import { getThings } from '../actions';
 
 class HelloWorld extends React.Component {
   render () {
@@ -30,7 +10,7 @@ class HelloWorld extends React.Component {
 
     const thingsList = things.map((thing) => {
       return (
-        <List.Item>
+        <List.Item key={thing.name}>
           <List.Icon name='arrow circle right' size='large' verticalAlign='middle' />
           <List.Content>
             <List.Header as='a'>{thing.name}</List.Header>
--- app/javascript/configureStore.js
+++ app/javascript/configureStore.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from "./reducers/index";
 
 const initialState = { 
   things: [
@@ -10,15 +11,6 @@ const initialState = {
     }
   ] 
 };
-
-function rootReducer(state, action) {
-  console.log(action.type);
-  switch (action.type) {
-    case "GET_THINGS_SUCCESS": 
-      return { things: action.json.things };
-  }
-  return state;
-}
 
 export default function configureStore() {
   const store = createStore(
